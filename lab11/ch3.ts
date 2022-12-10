89dfe9fd9885cf8ce90e28ed0a42a5ff03851c22
zion4h <21210240206@m.fudan.edu.cn>
2022-12-10 17:46:05 +0800

    lab1.2 complete chapter 3

---
+++ lab11/ch3.ts
@@ -0,0 +1,96 @@
+import {list, log, List, cons, none, node, Node} from "./util"
+
+// 常用函数
+const add = (a:number)=>(b:number):number => a + b
+const double = (n: number) => 2*n
+
+// foldr
+const foldr = <T>(f: (a: any)=>(b: T)=>T, x: T) => (l: List<any>):T => {
+    if (l.tag === "none")
+        return x
+    else 
+        return f(l.h)(foldr(f, x)(l.t))
+}
+const sum = foldr(add, 0)
+
+// append
+const append = <T>(a: List<T>)=>(b: List<T>): List<T> => foldr(cons, b)(a)
+
+// length
+const count = <T>(a: T)=>(n: number) => n + 1
+const length = foldr(count, 0)
+
+// doubleall
+// Old implemention:
+// const fandcons = (f: any) => compose(cons)(f)
+// const doubleandcons = fandcons(double)
+// const doubleall = foldr(doubleandcons, none())
+const compose = (f1: any)=>(f2: any) => (x: any) => f1(f2(x))
+const map = (f: any) => foldr(compose(cons)(f), none())
+const doubleall = map(double)
+
+// summatrix
+const summatrix = compose(sum)(map(sum))
+
+// TEST PART1: 
+// test a list of functions about structure list
+const Xs = list(1, 1, 2, 3, 5, 8, 13, 21)
+const Z1 = list(1, 2)
+const Z2 = list(3, 4)
+const M1 = list(Z1, Z2, Xs)
+log("Xs is " + Xs.toString()) 
+// Output:Xs is List[1, 1, 2, 3, 5, 8, 13, 21]
+log("return: ", sum(Xs))
+// Output:return:  54
+log("append", Z1.toString(), "and", Z2.toString(), "is", append(Z1)(Z2).toString())
+// Output:append List[1, 2] and List[3, 4] is List[1, 2, 3, 4]
+log("count", Xs.toString(), "is", length(Xs))
+// Output:count List[1, 1, 2, 3, 5, 8, 13, 21] is 8
+log("doubleall", Xs.toString(), "is", doubleall(Xs).toString())
+// Output:doubleall List[1, 1, 2, 3, 5, 8, 13, 21] is List[2, 2, 4, 6, 10, 16, 26, 42]
+log("summatrix", M1.toString(), "is", summatrix(M1))
+// Output:summatrix List[List[1, 2], List[3, 4], List[1, 1, 2, 3, 5, 8, 13, 21]] is 64
+
+// tree
+// 重写了下node方便造数据来测试
+const tree = <T>(h: T) => (...subtrees: Node<T>[]) => new Node(h, list(...subtrees))
+
+// folctree
+const foldtree = <T>(f: any, g:any, a:T)=>(l: List<any>):T=>{
+    if (l.tag === "none")
+        return a
+    else if (l.tag === "tree")
+        return f(l.h)(foldtree(f, g, a)(l.t))
+    else
+        return g(foldtree(f, g, a)(l.h))(foldtree(f, g, a)(l.t))
+}
+
+// sumtree
+const sumtree = foldtree(add, add, 0)
+
+// labels
+const labels = foldtree(cons, append, none())
+
+// maptree
+const maptree = (f: any)=> foldtree(compose(node)(f), cons, none())
+// doubletree
+const doubletree = maptree(double)
+
+const tree4 = tree(4)()
+const tree3 = tree(3)(tree4)
+const tree2 = tree(2)()
+const tree1 = tree(1)(tree2, tree3)
+log(tree1.toString())
+log(tree2.toString())
+log(tree3.toString())
+log(tree4.toString())
+// Node 1(List[Node 2, Node 3(List[Node 4])])
+// Node 2
+// Node 3(List[Node 4])
+// Node 4
+log(sumtree(tree1))
+// Output: 10
+log(labels(tree1).toString())
+// Output:List[1, 2, 3, 4]
+log(doubletree(tree1).toString())
+// Output:Node 2(List[Node 4, Node 6(List[Node 8])])
--- lab11/util.ts
+++ lab11/util.ts
@@ -1,27 +1,38 @@
 //  简化打印语句，方便调试
-export const log = (msg?: any, ...optionalParams: any[]) => 
-    console.log(msg, ...optionalParams)
+export const log = (msg?: any, ...optionalParams: any[]) => console.log(msg, ...optionalParams)
 
 abstract class ListBase<A> {
+    // 可视化打印
     toString(this:List<A>): string {
-        var msg = "List["   
+        var msg = ""   
         switch (this.tag) {
-            case "none": break
+            case "none": {
+                msg = "List[]"
+                break
+            }
             case "cons": {
-                msg += this.h
+                msg = "List[" + this.h
                 var cur = this.t
                 while (cur.tag === 'cons') {
                     msg += ", " + cur.h
                     cur = cur.t
                 }
+                msg += "]"
+                break
+            }
+            case "tree": {
+                msg += "Node " + this.h
+                if (this.t.tag === 'cons') {
+                    msg += "(" + this.t.toString() + ")"
+                } 
+                break
             }
         }
-        msg += "]"
         return msg
     }
 }
 
-export type List<T> = Nil<T> | Cons<T>
+export type List<T> = Nil<T> | Cons<T> | Node<T>
 export class Nil<T> extends ListBase<T> {
     // 方便类型推断的特殊技巧
     readonly tag: "none" = "none"
@@ -46,43 +57,19 @@ export const list = <T>(...s: T[]): List<T> => {
     if (s.length === 0)
       return none()
     else
-      return cons(s[0], list(...s.slice(1)))
+      return cons(s[0])(list(...s.slice(1)))
 }
-export const cons = <T>(h: T, t: List<T>): List<T> => new Cons(h, t)
+export const cons = <T>(h: T) => (t: List<T>) => new Cons(h, t)
 export const none = <T>(): List<T> => Nil.Nil
 
-const Xs = list(1, 1, 2, 3, 5, 8, 13, 21)
-log("Xs is " + Xs.toString()) 
-// Output:Xs is List[1, 1, 2, 3, 5, 8, 13, 21]
 
-const foldr = <T>(f: (a: any, b: T)=>T, x: T) => (l: List<any>):T => {
-    if (l.tag === "none")
-      return x
-    else return f(l.h, foldr(f, x)(l.t))
+export class Node<T> extends ListBase<T>{
+    readonly tag: "tree" = "tree"
+
+    constructor(readonly h: T, readonly t: List<Node<T>>) {
+        super()
+    }
 }
-const add = (a:number, b:number):number => a + b
-const sum = foldr(add, 0)
-log("return: ", sum(Xs))
-// Output:return:  54
 
-const Z1 = list(1, 2)
-const Z2 = list(3, 4)
-const append = <T>(a: List<T>, b: List<T>): List<T> => {
-    return foldr(cons, b)(a)
-}
-log("append", Z1.toString(), "and", Z2.toString(), "is", append(Z1, Z2).toString())
-// Output:append List[1, 2] and List[3, 4] is List[1, 2, 3, 4]
-
-const count = <T>(a: T, n: number) => n + 1
-const length = foldr(count, 0)
-log("count", Xs.toString(), "is", length(Xs))
-// Output:count List[1, 1, 2, 3, 5, 8, 13, 21] is 8
-
-const doubleandcons = (n: number, list: List<number>) => {
-    return cons(2 * n, list)
-}
-const doubleall = foldr(doubleandcons, none())
-log("doubleallcons", Xs.toString(), "is", doubleall(Xs).toString())
-// Output:doubleallcons List[1, 1, 2, 3, 5, 8, 13, 21] is List[2, 2, 4, 6, 10, 16, 26, 42]
-
+export const node = <T>(h: T) => (l: List<Node<T>>): Node<T> => new Node(h, l)
 
